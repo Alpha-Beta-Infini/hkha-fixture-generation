accdf191f732375f0d183e7f15a6dc2d43abd79d
Kris Stern <krisastern@gmail.com>
2020-11-09 11:48:29 +0800

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -15,8 +15,8 @@ def create_app():
     db.init_app(app)
 
     login_manager = LoginManager()
+    login_manager.init_app(app) 
     login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
 
     from .models import User
 
