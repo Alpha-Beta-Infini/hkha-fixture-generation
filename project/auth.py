fd9a74b5d9ba1bb3b6ddd72fe6cd08c62643a7a9
Kris Stern <krisastern@gmail.com>
2020-11-09 12:27:26 +0800

    Update auth.py

--- project/auth.py
+++ project/auth.py
@@ -25,7 +25,7 @@ def login_post():
     # take the user supplied password, hash it, and compare it to the hashed password in database
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login'))  # if user doesn't exist or password is wrong, reload the page
+        return redirect('login.html'))  # if user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
@@ -49,7 +49,7 @@ def signup_post():
 
     if user:  # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')
-        return redirect(url_for('auth.signup'))
+        return redirect('signup.html')
 
     # create new user with the form data. Hash the password so plaintext version isn't saved.
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
