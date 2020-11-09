c0393cbc3f8c44b89ecb5d89b32a3818f8ca509f
Kris Stern <krisastern@gmail.com>
2020-11-09 12:34:21 +0800

    Update main.py

--- project/main.py
+++ project/main.py
@@ -80,6 +80,66 @@ def scheduler():
     return render_template('scheduler.html')
 
 
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@main.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if user actually exists
+    # take the user supplied password, hash it, and compare it to the hashed password in database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect('login.html'))  # if user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return render_template('profile.html')
+
+
+@main.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@main.route('/signup', methods=['POST'])
+def signup_post():
+
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    # if this returns a user, then the email already exists in database
+    user = User.query.filter_by(email=email).first()
+
+    if user:  # if a user is found, we want to redirect back to signup page so user can try again
+        flash('Email address already exists')
+        return redirect('signup.html')
+
+    # create new user with the form data. Hash the password so plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('login.html')
+
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('index.html')
+
+
 @main.route('/timetable', methods=['GET', 'POST'])
 def timetable():
     if request.method == 'POST':
